1881300d20823c9729983fe45e50c2334c8e17bd
Hassaan Shakeel <hassaan.shakeel@docusign.com>
2018-04-24 11:51:29 -0700

    second commit

---
+++ Example/build/Pods.build/Debug-iphoneos/Pods-demo2Pod_Tests.build/DerivedSources/Pods_demo2Pod_Tests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_demo2Pod_TestsVersionString[];
+ extern const double Pods_demo2Pod_TestsVersionNumber;
+
+ const unsigned char Pods_demo2Pod_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_demo2Pod_Tests  PROJECT:Pods-1" "\n";
+ const double Pods_demo2Pod_TestsVersionNumber __attribute__ ((used)) = (double)1.;
