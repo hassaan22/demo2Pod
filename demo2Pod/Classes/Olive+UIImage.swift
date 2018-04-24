3129350bb01643f38695bd5de7191a5b2d9e18af
Hassaan Shakeel <hassaan.shakeel@docusign.com>
2018-04-24 14:01:34 -0700

    bundle works now

---
+++ demo2Pod/Classes/Olive+UIImage.swift
@@ -0,0 +1,12 @@
+//
+//  Olive+UIImage.swift
+//  Pods
+//
+//  Created by Hassaan Shakeel on 4/24/18.
+//
+
+import UIKit
+
+class Olive_UIImage: NSObject {
+
+}
---
+++ demo2Pod/Classes/Olive.swift
@@ -0,0 +1,12 @@
+//
+//  Olive.swift
+//  Pods
+//
+//  Created by Hassaan Shakeel on 4/24/18.
+//
+
+import UIKit
+
+class Olive: NSObject {
+
+}
